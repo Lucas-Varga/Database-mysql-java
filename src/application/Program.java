ee99624505a6b9750c97cc066084c353226ab8bd
vargalb <lucasvargalvl@gmail.com>
2026-06-29 14:01:23 -0300

    Criado instrução para incrementar tabela do banco de dados unitariamente ou mais de um dado de uma vez

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,19 +2,20 @@ package application;
 
 import db.DB;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Program {
     static void main() {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        /*recuperar dados do banco
         Connection conn = null;
         Statement st = null;
         ResultSet rs = null;
 
 
-        //recuperar dados do banco
+
         try {
             conn = DB.getConnection();
             st = conn.createStatement();
@@ -32,6 +33,53 @@ public class Program {
             DB.closeStatement(st);
             DB.closeConnection();
 
+        } */
+
+        Connection conn = null;
+        PreparedStatement st = null;
+        try {
+            conn = DB.getConnection();
+            /*
+            //inserindo dados na tabela
+            st = conn.prepareStatement(
+                    "INSERT INTO seller"
+                            + "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
+                            + "VALUES "
+                            + "(?,?,?,?,?)",
+                    Statement.RETURN_GENERATED_KEYS);
+
+            st.setString(1, "Carl Purple");
+            st.setString(2, "carl@email.com");
+            st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
+            st.setDouble(4, 3000.0);
+            st.setInt(5, 4);
+            */
+
+            // Inserindo mais de um dado na tabela
+            st = conn.prepareStatement(
+                    "insert into department (Name) values ('d2'),('D2')",
+                    Statement.RETURN_GENERATED_KEYS);
+
+            int rowAffected = st.executeUpdate();
+
+            if (rowAffected > 0) {
+                ResultSet rs = st.getGeneratedKeys();
+                while (rs.next()) {
+                    int id = rs.getInt(1);
+                    System.out.println("Done! Id = " + id);
+                }
+            } else {
+                System.out.println("No rown affected!");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        } /*catch (ParseException e) {
+            e.printStackTrace();
+        } */ finally {
+            DB.closeStatemant(st);
+            DB.closeConnection();
         }
 
     }
